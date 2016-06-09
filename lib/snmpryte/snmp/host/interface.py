1ae2e2883c073b87e59f9d05ea8eb841983d9567
Stephen Fromm <stephenf@nero.net>
2016-06-09 11:15:19 -0700

    flesh out host/interface
    - inherit from HostSystem
    - flesh out what is collected
    - other misc improvements

--- lib/snmpryte/snmp/host/interface.py
+++ lib/snmpryte/snmp/host/interface.py
@@ -18,10 +18,12 @@
 
 import logging
 import snmpryte.snmp
+from snmpryte.snmp.host import HostSystem
+from snmpryte.utils import *
 
-class HostInterface():
+class HostInterface(HostSystem):
 
-    CONF = {
+    DATA = {
         'ifIndex'       : '1.3.6.1.2.1.2.2.1.1',
         'ifDescr'       : '1.3.6.1.2.1.2.2.1.2',
         'ifType'        : '1.3.6.1.2.1.2.2.1.3',
@@ -36,30 +38,71 @@ class HostInterface():
     }
 
     STAT = {
-        'ifInUcastPkts'     : '1.3.6.1.2.1.2.2.1.11',
-        'ifInNUcastPkts'    : '1.3.6.1.2.1.2.2.1.12',
-        'ifInDiscards'      : '1.3.6.1.2.1.2.2.1.13',
-        'ifInErrors'        : '1.3.6.1.2.1.2.2.1.14',
-        'ifInUnknownProtos' : '1.3.6.1.2.1.2.2.1.15',
-        'ifOutUcastPkts'    : '1.3.6.1.2.1.2.2.1.17',
-        'ifOutNUcastPkts'   : '1.3.6.1.2.1.2.2.1.18',
-        'ifOutDiscards'     : '1.3.6.1.2.1.2.2.1.19',
-        'ifOutErrors'       : '1.3.6.1.2.1.2.2.1.20',
-        'ifOutQLen'         : '1.3.6.1.2.1.2.2.1.21',
+        'ifInNUcastPkts'       : '1.3.6.1.2.1.2.2.1.12',
+        'ifInDiscards'         : '1.3.6.1.2.1.2.2.1.13',
+        'ifInErrors'           : '1.3.6.1.2.1.2.2.1.14',
+        'ifInUnknownProtos'    : '1.3.6.1.2.1.2.2.1.15',
+        'ifOutNUcastPkts'      : '1.3.6.1.2.1.2.2.1.18',
+        'ifOutDiscards'        : '1.3.6.1.2.1.2.2.1.19',
+        'ifOutErrors'          : '1.3.6.1.2.1.2.2.1.20',
+        'ifOutQLen'            : '1.3.6.1.2.1.2.2.1.21',
+        'ifInMulticastPkts'    : '1.3.6.1.2.1.31.1.1.1.2',
+        'ifInBroadcastPkts'    : '1.3.6.1.2.1.31.1.1.1.3',
+        'ifOutMulticastPkts'   : '1.3.6.1.2.1.31.1.1.1.4',
+        'ifOutBroadcastPkts'   : '1.3.6.1.2.1.31.1.1.1.5',
+        'ifHCInOctets'         : '1.3.6.1.2.1.31.1.1.1.6',
+        'ifHCInUcastPkts'      : '1.3.6.1.2.1.31.1.1.1.7',
+        'ifHCInMulticastPkts'  : '1.3.6.1.2.1.31.1.1.1.8',
+        'ifHCInBroadcastPkts'  : '1.3.6.1.2.1.31.1.1.1.9',
+        'ifHCOutOctets'        : '1.3.6.1.2.1.31.1.1.1.10',
+        'ifHCOutUcastPkts'     : '1.3.6.1.2.1.31.1.1.1.11',
+        'ifHCOutMulticastPkts' : '1.3.6.1.2.1.31.1.1.1.12',
+        'ifHCOutBroadcastPkts' : '1.3.6.1.2.1.31.1.1.1.13',
     }
 
-    CONVERSION = { }
+    NAME = 'interface'
+
+    XLATE = {
+        'ifHC' : '',
+        'if'   : '',
+    }
+
+    CONVERSION = {
+        'ifAdminStatus': {
+            1 : 'up',
+            2 : 'down',
+            3 : 'testing',
+        },
+        'ifOperStatus': {
+            1 : 'up',
+            2 : 'down',
+            3 : 'testing',
+            4 : 'unknown',
+            5 : 'dormant',
+            6 : 'notPresent',
+            7 : 'lowerLayerDown',
+        },
+    }
 
     def __init__(self, snmp):
         self.snmp = snmp
-        self._interfaces = self._get_configuration()
+        super(HostInterface, self).__init__(snmp)
+        logging.info("inspecting %s for interface data", snmp.host)
+        self._data = self._get_interface()
 
-    def _get_configuration(self):
-        return snmpryte.snmp.get_snmp_data(self.snmp, HostInterface.CONF, HostInterface.CONVERSION)
+    def _get_interface(self):
+        data = snmpryte.snmp.get_snmp_data(self.snmp, HostInterface.DATA, HostInterface.CONVERSION)
+        stat = snmpryte.snmp.get_snmp_data(self.snmp, HostInterface.STAT, HostInterface.CONVERSION)
+        merge_dicts(data, stat)
+        return data
+
+    @property
+    def data(self):
+        return self._data
 
     @property
     def interfaces(self):
-        return self._interfaces
+        return self.data
 
     def get_interface_stats(self):
-        pass
+        return snmpryte.snmp.get_snmp_data(self.snmp, HostInterface.STAT, HostInterface.CONVERSION)
