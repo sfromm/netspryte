b14f94157ab66d6aea6148b39235b87c9deb3294
Stephen Fromm <stephenf@nero.net>
2016-06-09 14:49:40 -0700

    rework collectsnmp to allow multiprocess pools
    create a pool of workers to handle workload of collecting data from devices

--- lib/snmpryte/commands/collectsnmp.py
+++ lib/snmpryte/commands/collectsnmp.py
@@ -51,89 +51,99 @@ class CollectSnmpCommand(BaseCommand):
         setup_logging(args.verbose)
         logging.debug("beginning data collection")
         cfg = C.load_config()
+        pool = Pool(processes=C.DEFAULT_WORKERS)
+        logging.debug("created pool of workers")
         for device in args.devices:
-            msnmp = snmpryte.snmp.SNMPSession(host=device)
-            try:
-                cbqos = CiscoCBQOS(msnmp)
-                self.process_policers(cbqos, args)
-                self.process_interfaces(cbqos, args)
-            except TypeError as e:
-                logging.error("encountered error with %s; skipping to next device: %s", device, str(e))
+            logging.info("handing off %s to worker", device)
+            res = pool.apply_async(process_device, (device, args))
+        pool.close()
+        logging.debug("closed pool of workers")
+        pool.join()
 
-    def process_policers(self, cbqos, args):
-        for key, data in cbqos.data.iteritems():
-            if 'cbQosObjectsType' not in data:
-                continue
-            if data['cbQosObjectsType'] != 'police':
-                continue
-            # process the policer data
-            conf = self.get_data(CiscoCBQOS.DATA, data)
-            conf['_class'] = CiscoCBQOS.NAME
-            conf['_idx'] = key
-            conf['_title'] = cbqos.get_policy_map_name(key)
-            profile_stat = CiscoCBQOS.STAT.copy()
-            profile_stat.update(HostInterface.STAT)
-            for k in CiscoCBQOS.DATA.keys():
-                if k in data and snmpryte.snmp.value_is_metric(data[k]):
-                    if 'Rate' in k:
-                        profile_stat[k] = data[k]
-            profile_strip = CiscoCBQOS.XLATE.copy()
-            profile_strip.update(HostInterface.XLATE)
-            values = self.get_data(profile_stat, data, profile_strip)
-            self.process_data_instance(cbqos, args, CiscoCBQOS.NAME, key, conf, values)
 
-    def process_interfaces(self, cbqos, args):
-        for key, data in cbqos.interfaces.iteritems():
-            conf = self.get_data(HostInterface.DATA, data)
-            conf['_class'] = HostInterface.NAME
-            conf['_idx'] = key
-            conf['_title'] = conf.get('ifAlias', '')
-            if not conf['_title']:
-                conf['_title'] = conf.get('ifDescr', 'NA')
+def process_device(device, args):
+    try:
+        msnmp = snmpryte.snmp.SNMPSession(host=device)
+        cbqos = CiscoCBQOS(msnmp)
+        process_policers(cbqos, args)
+        process_interfaces(cbqos, args)
+    except TypeError as e:
+        logging.error("encountered error with %s; skipping to next device: %s", device, str(e))
 
-            values = self.get_data(HostInterface.STAT, data, HostInterface.XLATE)
-            self.process_data_instance(cbqos, args, HostInterface.NAME, key, conf, values)
+def process_policers(cbqos, args):
+    for key, data in cbqos.data.iteritems():
+        if 'cbQosObjectsType' not in data:
+            continue
+        if data['cbQosObjectsType'] != 'police':
+            continue
+        # process the policer data
+        conf = get_data(CiscoCBQOS.DATA, data)
+        conf['_class'] = CiscoCBQOS.NAME
+        conf['_idx'] = key
+        conf['_title'] = cbqos.get_policy_map_name(key)
+        profile_stat = CiscoCBQOS.STAT.copy()
+        profile_stat.update(HostInterface.STAT)
+        for k in CiscoCBQOS.DATA.keys():
+            if k in data and snmpryte.snmp.value_is_metric(data[k]):
+                if 'Rate' in k:
+                    profile_stat[k] = data[k]
+        profile_strip = CiscoCBQOS.XLATE.copy()
+        profile_strip.update(HostInterface.XLATE)
+        values = get_data(profile_stat, data, profile_strip)
+        process_data_instance(cbqos, args, CiscoCBQOS.NAME, key, conf, values)
 
-    def process_data_instance(self, cbqos, args, name, key, conf, values):
-        ''' '''
-        if not values:
-            return
-        if args.data:
-            if name == 'interface':
-                pprint.pprint({key: cbqos.interfaces[key]})
-            else:
-                pprint.pprint({key: cbqos.policers[key]})
-        json_path = mk_json_filename(cbqos, name, key)
-        db = get_db_backend()
-        if db.backend == 'rrd':
-            db.path = json_path.replace('json', 'rrd')
-        self.record_stats(db, values)
-        self.record_data(json_path, conf, args)
+def process_interfaces(cbqos, args):
+    for key, data in cbqos.interfaces.iteritems():
+        conf = get_data(HostInterface.DATA, data)
+        conf['_class'] = HostInterface.NAME
+        conf['_idx'] = key
+        conf['_title'] = conf.get('ifAlias', '')
+        if not conf['_title']:
+            conf['_title'] = conf.get('ifDescr', 'NA')
 
-    def record_data(self, path, data, args):
-        path = os.path.join(args.datadir, path)
-        data2 = { k: snmpryte.snmp.mk_pretty_value(v) for (k, v) in data.iteritems() }
-        if '_do_graph' not in data2:
-            data2['_do_graph'] = True
-        if os.path.exists(path):
-            return
-        snmpryte.utils.json2path(data2, path)
+        values = get_data(HostInterface.STAT, data, HostInterface.XLATE)
+        process_data_instance(cbqos, args, HostInterface.NAME, key, conf, values)
 
-    def record_stats(self, db, data):
-        db.write(data)
+def process_data_instance(cbqos, args, name, key, conf, values):
+    ''' '''
+    if not values:
+        return
+    if args.data:
+        if name == 'interface':
+            pprint.pprint({key: cbqos.interfaces[key]})
+        else:
+            pprint.pprint({key: cbqos.policers[key]})
+    json_path = mk_json_filename(cbqos, name, key)
+    db = get_db_backend()
+    if db.backend == 'rrd':
+        db.path = json_path.replace('json', 'rrd')
+    record_stats(db, values)
+    record_data(json_path, conf, args)
 
-    def get_data(self, template, data, xlate={}):
-        values = dict()
-        for k in template:
-            newk = self.clean_name(k, xlate)
-            if k in data:
-                values[newk] = data[k]
-            else:
-                values[newk] = 0
-        return values
+def record_data(path, data, args):
+    path = os.path.join(args.datadir, path)
+    data2 = { k: snmpryte.snmp.mk_pretty_value(v) for (k, v) in data.iteritems() }
+    if '_do_graph' not in data2:
+        data2['_do_graph'] = True
+    if os.path.exists(path):
+        return
+    snmpryte.utils.json2path(data2, path)
 
-    def clean_name(self, name, xlate):
-        for k,v in xlate.iteritems():
-            name = name.replace(k, v, 1)
-        return name
+def record_stats(db, data):
+    db.write(data)
+
+def get_data(template, data, xlate={}):
+    values = dict()
+    for k in template:
+        newk = clean_name(k, xlate)
+        if k in data:
+            values[newk] = data[k]
+        else:
+            values[newk] = 0
+    return values
+
+def clean_name(name, xlate):
+    for k,v in xlate.iteritems():
+        name = name.replace(k, v, 1)
+    return name
 
