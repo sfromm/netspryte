98ad2e6e5bbeed0f18dc4eed01579b5dac4c92d9
Stephen Fromm <stephenf@nero.net>
2016-06-21 10:01:57 -0700

    updates to mkgraphs
    lot of work to improve split between data/www, concurrent processing, etc

--- lib/snmpryte/commands/mkgraphs.py
+++ lib/snmpryte/commands/mkgraphs.py
@@ -40,19 +40,20 @@ class MkGraphsCommand(BaseCommand):
         super(MkGraphsCommand, self).__init__(daemonize)
         self.parser.add_argument('globs', type=str, nargs='*',
                                  help='list of directory globs to graph')
-        self.parser.add_argument('--wwwdir', default=C.DEFAULT_WWWDIR,
-                                 help='Path to www directory')
 
     def run(self):
         args = self.parser.parse_args()
         if not args.datadir or not os.path.exists(args.datadir):
             logging.error("Path to data directory does not exist: %s", args.datadir)
             return 1
+        if not C.DEFAULT_WWWDIR or not os.path.exists(C.DEFAULT_WWWDIR):
+            logging.error("Path to data directory does not exist: %s", C.DEFAULT_WWWDIR)
+            return 1
         setup_logging(args.verbose)
         logging.debug("beginning graph creation")
         cfg = C.load_config()
         data_globs = list()
-        data_graphs = list()
+        data_set = list()
         if args.globs:
             for dev in args.globs:
                 data_globs.extend(
@@ -62,48 +63,48 @@ class MkGraphsCommand(BaseCommand):
             data_globs.extend(
                 glob.glob("{0}/*/*.json".format(args.datadir))
             )
-        for json_path in data_globs:
-            d = self.graph_data_instance(args, json_path, cfg)
-            data_graphs.append(d)
-        self.template_html(args.datadir, cfg, data_graphs)
-
-    def graph_data_instance(self, args, json_path, cfg):
-        data_set = parse_json_from_file(json_path)
-        rrd_path = json_path.replace('json', 'rrd')
-        if self.skip_data_instance(data_set):
-            logging.debug("skipping graph generation of %s", rrd_path)
-            return data_set
-        data_set['_graphs'] = list()
-        base_rrd_opts = list()
-        logging.debug("graphing rrd %s", rrd_path)
-        section = "rrd_{0}".format(data_set['_class'])
-        graphs = C.get_config(cfg, section, 'graph', None, None, islist=True)
         pool = Pool(processes=C.DEFAULT_WORKERS)
         logging.debug("created pool of workers")
-        for graph in graphs:
-            graph_defs = self.graph_data_definitions(data_set, graph, rrd_path, cfg)
-            for graph_def in graph_defs:
-                if args.nofork:
-                    pool.apply_async(rrd_graph, (graph_def[0], graph_def[1], graph_def[2]))
-                else:
-                    rrd_graph(graph_def[0], graph_def[1], graph_def[2])
-                data_set['_graphs'].append(self.get_relative_png_path(graph_def[0]))
+        for json_path in sorted(data_globs):
+            (data, graph_defs) = self.graph_data_instance(args, json_path, cfg)
+            data_set.append(data)
+            if args.nofork:
+                do_graph_instance(graph_defs)
+            else:
+                res = pool.apply_async(do_graph_instance, (graph_defs,))
         pool.close()
         logging.debug("closed pool of workers")
         pool.join()
-        return data_set
+        self.template_html(C.DEFAULT_WWWDIR, cfg, data_set)
 
-    def skip_data_instance(self, data_set):
-        if '_do_graph' not in data_set:
+    def graph_data_instance(self, args, json_path, cfg):
+        data = parse_json_from_file(json_path)
+        rrd_path = json_path.replace('json', 'rrd')
+        if self.skip_data_instance(data):
+            logging.debug("skipping graph generation of %s", rrd_path)
+            return data
+        data['_graphs'] = list()
+        base_rrd_opts = list()
+        graph_defs = list()
+        section = "rrd_{0}".format(data['_class'])
+        graphs = C.get_config(cfg, section, 'graph', None, None, islist=True)
+        for graph in graphs:
+            graph_defs.extend(self.graph_data_definitions(data, graph, rrd_path, cfg))
+            for graph_def in graph_defs:
+                data['_graphs'].append(get_relative_png_path(graph_def[0]))
+        return (data, graph_defs)
+
+    def skip_data_instance(self, data):
+        if '_do_graph' not in data:
             return True
-        elif not data_set['_do_graph']:
+        elif not data['_do_graph']:
             return True
-        elif '_class' not in data_set:
+        elif '_class' not in data:
             return True
         else:
             return False
 
-    def get_graph_opts(self, cfg):
+    def get_graph_cmd_opts(self, cfg):
         base_rrd_opts = list()
         for name, val in cfg.items('rrd'):
             if name in ['start', 'step', 'heartbeat']:
@@ -115,7 +116,7 @@ class MkGraphsCommand(BaseCommand):
 
     def graph_data_definitions(self, data_set, graph, rrd_path, cfg):
         graph_opts = list()
-        rrd_opts = self.get_graph_opts(cfg)
+        rrd_opts = self.get_graph_cmd_opts(cfg)
         for name, val in cfg.items(graph):
             if name in ['def', 'cdef', 'vdef', 'graph']:
                 for opt in C.get_config(cfg, graph, name, None, None, islist=True):
@@ -136,33 +137,22 @@ class MkGraphsCommand(BaseCommand):
         start_periods = C.get_config(cfg, 'rrd', 'start', None, None, islist=True)
         for period in start_periods:
             graph_defs.append( (
-                self.get_absolute_png_path(rrd_path, graph, period),
+                get_absolute_png_path(rrd_path, graph, period),
                 rrd_opts + ['--start', period],
                 graph_opts
             ) )
         return graph_defs
 
-    def get_absolute_png_path(self, rrd_path, graph, period):
-        png_path_list = rrd_path.rsplit('.', 1)
-        png_path_list[0] += "-{0}".format(graph)
-        png_path_list[0] += period
-        png_path_list[-1] = 'png'
-        return ".".join(png_path_list)
-
-    def get_relative_png_path(self, arg):
-        head, tail = os.path.split(arg)
-        subdir = os.path.basename(head)
-        return os.path.join(subdir, tail)
-
-    def do_graph_data(self, png_path, rrd_opts, graph_opts):
-        rrd_graph(png_path, rrd_opts, graph_opts)
-
-    def template_html(self, datadir, cfg, data_graphs):
+    def template_html(self, wwwdir, cfg, data_set):
+        graph_periods = list()
         data_class = dict()
+        start_periods = C.get_config(cfg, 'rrd', 'start', None, None, islist=True)
+        graph_periods.append(start_periods[0])
+        detail_graph_periods = [ x for x in start_periods if x not in graph_periods ]
         env = Environment(
-            loader=FileSystemLoader(datadir)
+            loader=FileSystemLoader(wwwdir)
         )
-        for k in data_graphs:
+        for k in data_set:
             if k['_class'] not in data_class:
                 data_class[k['_class']] = list()
             data_class[k['_class']].append(k)
@@ -171,15 +161,46 @@ class MkGraphsCommand(BaseCommand):
             self.do_template_html(
                 env,
                 C.get_config(cfg, 'general', 'html_template', None, None),
-                os.path.join(datadir, "{0}.html".format(cls)),
-                sorted(data_class[cls], key=lambda k: k['_title'])
+                os.path.join(wwwdir, "{0}.html".format(cls)),
+                sorted(data_class[cls], key=lambda k: k['_title']),
+                graph_periods
+            )
+        for data in data_set:
+            title = clean_string(data['_id'])
+            self.do_template_html(
+                env,
+                C.get_config(cfg, 'general', 'html_template', None, None),
+                os.path.join(wwwdir, "{0}.html".format(title)),
+                [data],
+                detail_graph_periods
             )
 
-    def do_template_html(self, env, template, path, data_graphs):
+    def do_template_html(self, env, template, path, data_set, graph_periods):
         try:
             logging.info("creating html template %s", path)
             template = env.get_template(template)
-            data2path(template.render(data_sets=data_graphs), path)
+            data2path(template.render(data_sets=data_set, graph_periods=graph_periods), path)
             os.chmod(path, 0664)
         except jinja2.exceptions.TemplateNotFound as e:
             logging.error("failed to find template: %s", str(e))
+
+def get_absolute_png_path(rrd_path, graph, period):
+    basedir = os.path.basename( os.path.dirname(rrd_path) )
+    basename = os.path.basename(rrd_path)
+    basename_list = basename.rsplit('.', 1)
+    basename_list[0] += "-{0}".format(graph)
+    basename_list[0] += period
+    basename_list[-1] = 'png'
+    return os.path.join(C.DEFAULT_WWWDIR, basedir, ".".join(basename_list))
+
+def get_relative_png_path(arg):
+    head, tail = os.path.split(arg)
+    subdir = os.path.basename(head)
+    return os.path.join(subdir, tail)
+
+def do_graph_instance(graph_defs):
+    dbs = get_db_backend()
+    for db in dbs:
+        for graph_def in graph_defs:
+            if db.backend == 'rrd':
+                rrd_graph(graph_def[0], graph_def[1], graph_def[2])
