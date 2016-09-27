d50161f9f54678af6271f744e586f0f2a8412760
Stephen Fromm <stephenf@nero.net>
2016-09-27 10:42:23 -0700

    updates to mkhtml
    - write out a joined data state file for use by the cgi
    - write out device-specific pages
    - add method for sorting device instances

--- lib/netspryte/commands/mkhtml.py
+++ lib/netspryte/commands/mkhtml.py
@@ -53,12 +53,14 @@ class MkHtmlCommand(BaseCommand):
         logging.debug("beginning html creation")
         cfg = C.load_config()
         data_globs = list()
-        data_set = get_data_instances()
+        data_set = get_data_instances_from_disjoined()
+        json2path(data_set, os.path.join(C.DEFAULT_DATADIR, C.DEFAULT_DATA_JOINED))
         self.template_html(C.DEFAULT_WWWDIR, cfg, data_set)
 
     def template_html(self, wwwdir, cfg, data_set):
         graph_periods = list()
         data_class = dict()
+        data_device = dict()
 
         start_periods = C.get_config(cfg, 'rrd', 'start', None, None, islist=True)
         graph_periods.append(start_periods[0])
@@ -73,7 +75,14 @@ class MkHtmlCommand(BaseCommand):
                 data_class[k['_class']] = list()
             data_class[k['_class']].append(k)
             logging.debug("adding %s to data class %s", k['_id'], k['_class'])
+            device = k['_id'].split('.', 1)[0]
+            device = device.replace('_', '.')
+            if device not in data_device:
+                data_device[device] = list()
+            data_device[device].append(k)
+            logging.debug("adding %s to device %s", k['_id'], device)
 
+        # for each class of data
         for cls in data_class.keys():
             graph_defs = C.get_config(cfg, "rrd_{0}".format(cls), 'graph', None, None, islist=True)
             self.do_template_html(
@@ -84,6 +93,22 @@ class MkHtmlCommand(BaseCommand):
                 graph_periods,
                 [ graph_defs[0] ],
             )
+        # for each device
+        graph_defs = list()
+        for cls in data_class.keys():
+            graph_defs.extend(C.get_config(cfg, "rrd_{0}".format(cls), 'graph', None, None, islist=True))
+        for device in data_device.keys():
+            title = device
+
+            self.do_template_html(
+                env,
+                C.get_config(cfg, 'general', 'html_template', None, None),
+                os.path.join(wwwdir, "{0}.html".format(title)),
+                self.sort_device_data_instances(data_device[device]),
+                graph_periods,
+                graph_defs,
+            )
+        # for each data instance
         for data in data_set:
             title = clean_string(data['_id'])
             graph_defs = C.get_config(cfg, "rrd_{0}".format(cls), 'graph', None, None, islist=True)
@@ -108,3 +133,14 @@ class MkHtmlCommand(BaseCommand):
             os.chmod(path, 0664)
         except jinja2.exceptions.TemplateNotFound as e:
             logging.error("failed to find template: %s", str(e))
+
+    def sort_device_data_instances(self, devlist):
+        data_class = dict()
+        instances = list()
+        for inst in devlist:
+            if inst['_class'] not in data_class:
+                data_class[inst['_class']] = list()
+            data_class[inst['_class']].append(inst)
+        for dclass in sorted(data_class.keys()):
+            instances.extend( sorted(data_class[dclass], key=lambda k: float(k['_idx']) ) )
+        return instances
