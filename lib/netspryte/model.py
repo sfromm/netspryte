e9ebaaa0751bc412db508787682aaa359c5bf920
Stephen Fromm <stephenf@nero.net>
2019-02-28 16:52:05 -0800

    updates to model
    - remove attrs column
    - add attribute tables for collected measurement_instances

--- lib/netspryte/model.py
+++ lib/netspryte/model.py
@@ -17,26 +17,27 @@
 # along with netspryte.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
-import os
-import logging
-import subprocess
 
 from netspryte import constants as C
 
-from peewee import *
-from playhouse.postgres_ext import *
+from peewee import BigIntegerField, IntegerField, CharField, DateTimeField, ForeignKeyField, TextField, Proxy, Model
+from playhouse.postgres_ext import BinaryJSONField
+
 
 DB_PROXY = Proxy()
+
 
 class BaseModel(Model):
     class Meta:
         database = DB_PROXY
+
 
 class Netspryte(BaseModel):
     schema = IntegerField(default=1)
 
     class Meta:
         db_table = 'netspryte'
+
 
 class Host(BaseModel):
     name = CharField(index=True)
@@ -49,6 +50,29 @@ class Host(BaseModel):
 
     def __repr__(self):
         return '<Host: %s>' % self.name
+
+    def __str__(self):
+        return '<Host: %s>' % self.name
+
+class HostSnmpAttrs(BaseModel):
+    sysDescr = CharField(null=True)
+    sysObjectID = CharField()
+    sysUpTime = BigIntegerField(null=True)
+    sysContact = CharField(null=True)
+    sysName = CharField()
+    sysLocation = CharField(null=True)
+    sysServices = CharField(null=True)
+    host = ForeignKeyField(Host, related_name='host_snmp_attrs', null=False, on_delete='CASCADE')
+
+    class Meta:
+        db_table = 'host_snmp_attrs'
+
+    def __repr__(self):
+        return '<HostSnmpAttrs: %s>' % self.sysName
+
+    def __str__(self):
+        return '<HostSnmpAttrs: %s>' % self.sysName
+
 
 class MeasurementClass(BaseModel):
     name = CharField()
@@ -66,17 +90,18 @@ class MeasurementClass(BaseModel):
     def __repr__(self):
         return '<MeasurementClass: %s>' % self.name
 
+    def __str__(self):
+        return '<MeasurementClass: %s>' % self.name
+
+
 class MeasurementInstance(BaseModel):
     name = CharField(index=True, unique=True)
     index = CharField()
-    attrs = BinaryJSONField(null=True)
     metrics = BinaryJSONField(null=True)
     presentation = BinaryJSONField(null=True)
     lastseen = DateTimeField(default=datetime.datetime.now, index=True)
-    host = ForeignKeyField(Host,
-                           related_name='measurement_instances', null=False, on_delete='CASCADE')
-    measurement_class = ForeignKeyField(MeasurementClass,
-                                        related_name='measurement_instances', null=False, on_delete='CASCADE')
+    host = ForeignKeyField(Host, related_name='measurement_instances', null=False, on_delete='CASCADE')
+    measurement_class = ForeignKeyField(MeasurementClass, related_name='measurement_instances', null=False, on_delete='CASCADE')
 
     class Meta:
         db_table = "measurement_instance"
@@ -84,6 +109,10 @@ class MeasurementInstance(BaseModel):
 
     def __repr__(self):
         return '<MeasurementInstance: %s>' % self.name
+
+    def __str__(self):
+        return '<MeasurementInstance: %s>' % self.name
+
 
 class Tag(BaseModel):
     name = CharField()
@@ -94,6 +123,7 @@ class Tag(BaseModel):
     def __repr__(self):
         return '<Tag: %s>' % self.name
 
+
 class MeasurementInstanceTag(BaseModel):
     measurement_instance = ForeignKeyField(MeasurementInstance, backref='tags')
     tag = ForeignKeyField(Tag, backref='measurementinstances')
@@ -101,9 +131,103 @@ class MeasurementInstanceTag(BaseModel):
     class Meta:
         db_table = "measurement_instance_tag"
 
+
 class HostTag(BaseModel):
     host = ForeignKeyField(Host, backref='tags')
     tag = ForeignKeyField(Tag, backref='hosts')
 
     class Meta:
         db_table = "host_tag"
+
+
+class IPAddressAttrs(BaseModel):
+    """
+    A PeeWee model for a IP Network
+    """
+
+    ipAddressAddrType = CharField(null=True)
+    ipAddressAddr = CharField()
+    ipAddressIfIndex = IntegerField(null=True)
+    ipAddressType = CharField(null=True)
+    ipAddressPrefix = CharField(null=True)
+    ipAddressOrigin = CharField(null=True)
+    ipAddressStatus = CharField(null=True)
+    measurement_instance = ForeignKeyField(MeasurementInstance, related_name='ipaddress_attrs', null=False, on_delete='CASCADE')
+
+    class Meta:
+        db_table = "ipaddress_attrs"
+
+    def __repr__(self):
+        return '<IPAddressAttrs: %s>' % (self.ipAddressAddr)
+
+    def __str__(self):
+        return '<IPAddressAttrs: %s>' % (self.ipAddressAddr)
+
+
+class InterfaceAttrs(BaseModel):
+    ifIndex = IntegerField()
+    ifDescr = CharField(null=True)
+    ifType = IntegerField(null=True)
+    ifMtu = IntegerField(null=True)
+    ifSpeed = BigIntegerField(null=True)
+    ifPhysAddress = CharField(null=True)
+    ifAdminStatus = CharField(null=True)
+    ifOperStatus = CharField(null=True)
+    ifName = CharField(null=True)
+    ifHighSpeed = IntegerField(null=True)
+    ifAlias = TextField(null=True)
+    measurement_instance = ForeignKeyField(MeasurementInstance, related_name='interface_attrs', null=False, on_delete='CASCADE')
+
+    class Meta:
+        db_table = "interface_attrs"
+
+    def __repr__(self):
+        return '<InterfaceAttrs: %s:%s>' % (self.ifIndex, self.ifName)
+
+    def __str__(self):
+        return '<InterfaceAttrs: %s:%s>' % (self.ifIndex, self.ifName)
+
+class UPSAttrs(BaseModel):
+    upsIdentManufacturer = CharField()
+    upsIdentModel = CharField()
+    upsIdentUPSSoftwareVersion = CharField()
+    upsIdentAgentSoftwareVersion = CharField()
+    upsIdentName = CharField()
+    upsIdentAttachedDevices = CharField()
+    upsInputNumLines = CharField()
+    upsOutputSource = CharField()
+    upsOutputFrequency = IntegerField()
+    upsOutputNumLines = CharField()
+    measurement_instance = ForeignKeyField(MeasurementInstance, related_name='hostups_attrs', null=False, on_delete='CASCADE')
+
+    class Meta:
+        db_table = "hostups_attrs"
+
+    def __repr__(self):
+        return '<UPSAttrs: %s>' % self.upsIdentName
+
+class CBQOSAttrs(BaseModel):
+    cbQosIfType = CharField(null=True)
+    cbQosPolicyDirection = CharField(null=True)
+    cbQosIfIndex = IntegerField()
+    cbQosConfigIndex = CharField(null=True)
+    cbQosObjectsType = CharField()
+    cbQosParentObjectsIndex = CharField(null=True)
+    cbQosPolicyMapName = CharField(null=True)
+    cbQosCMName = CharField(null=True)
+    cbQosPoliceCfgBurstSize = IntegerField(null=True)
+    cbQosPoliceCfgExtBurstSize = CharField(null=True)
+    cbQosPoliceCfgConformAction = CharField(null=True)
+    cbQosPoliceCfgExceedAction = CharField(null=True)
+    cbQosPoliceCfgViolateAction = CharField(null=True)
+    cbQosPoliceCfgRate64 = BigIntegerField(null=True)
+    measurement_instance = ForeignKeyField(MeasurementInstance, related_name='cbqos_attrs', null=False, on_delete='CASCADE')
+
+    class Meta:
+        db_table = "cbqos_attrs"
+
+    def __repr__(self):
+        return '<CBQOSAttrs: %s>' % self.cbQosConfigIndex
+
+    def __str__(self):
+        return '<CBQOSAttrs: %s>' % self.cbQosConfigIndex
