614c34bdbc4722328956d043b928559a5377451d
Stephen Fromm <stephenf@nero.net>
2019-03-12 16:11:19 -0700

    updates to snmp data worker
    rename some methods to be more descriptive, similar to elsewhere.
    use process_measurement_instance_data to also handle metric data.

--- lib/netspryte/utils/worker.py
+++ lib/netspryte/utils/worker.py
@@ -52,7 +52,7 @@ class DataWorker(multiprocessing.Process):
             now = datetime.datetime.now()
             if not this_host:
                 this_host = self.mgr.get_or_create(netspryte.model.Host, name=data['host'])
-                self.process_host_attrs(data_mod, this_host)
+                self.process_host_data(data_mod, this_host)
             if not this_class:
                 this_class = self.mgr.get_or_create(netspryte.model.MeasurementClass, name=data['class'], transport=data['transport'])
             if this_host is None or this_class is None:
@@ -73,13 +73,13 @@ class DataWorker(multiprocessing.Process):
                 this_inst.presentation = json_ready(data['presentation'])
             # prepare metrics for
             if 'metrics' in data:
-                this_inst.metrics = json_ready(data['metrics'])
                 if not metric_types:
                     for k, v in list(data['metrics'].items()):
                         metric_types[k] = netspryte.snmp.get_value_type(v)
                     this_class.metric_type = json_ready(metric_types)
             self.mgr.save(this_inst)
-            self.process_data_instance_attrs(data, this_inst, data_mod.ATTR_MODEL)
+            self.process_measurement_instance_data(data['attrs'], this_inst, data_mod.ATTR_MODEL)
+            self.process_measurement_instance_data(data['metrics'], this_inst, data_mod.METRIC_MODEL)
             if this_inst.metrics:
                 has_metrics.append(this_inst)
         self.mgr.save(this_host)
@@ -88,34 +88,34 @@ class DataWorker(multiprocessing.Process):
         t.stop_timer()
         return has_metrics
 
-    def process_data_instance_attrs(self, data, measurement_instance, model):
+    def process_measurement_instance_data(self, data, measurement_instance, model):
         ''' Process measurement instance attributes '''
         logging.debug("adding/updating attributes for %s", measurement_instance.name)
-        init_attrs = dict()
-        attrs = json_ready(data['attrs'])
-        attrs['measurement_instance'] = measurement_instance
+        init_data = dict()
+        data = json_ready(data)
+        data['measurement_instance'] = measurement_instance
         if not hasattr(netspryte.model, model):
             logging.error("cannot find class model for %s attributes", measurement_instance.measurement_class.name)
             return None
         cls = getattr(netspryte.model, model)
-        # This is probably supposed to be handled by process_host_attrs()
+        # This is probably supposed to be handled by process_host_data()
         if not hasattr(cls, 'measurement_instance'):
             logging.info("skipping model %s that does not have relation to measurement_instance", model)
-            return
+            return None
         for field in cls._meta.sorted_fields:
-            if field.name == 'id' or isinstance(field, peewee.ForeignKeyField) or field.name not in attrs:
+            if field.name == 'id' or isinstance(field, peewee.ForeignKeyField) or field.name not in data:
                 continue
             if not field.null:
-                init_attrs[field.name] = attrs[field.name]
-        init_attrs['measurement_instance'] = measurement_instance
-        m = self.mgr.get_or_create(cls, **init_attrs)
+                init_data[field.name] = data[field.name]
+        init_data['measurement_instance'] = measurement_instance
+        m = self.mgr.get_or_create(cls, **init_data)
         if m is None:
             logging.error("failed to get or create attributes for measurement instance %s", measurement_instance)
             return None
-        self.mgr.update(m, **attrs)
+        self.mgr.update(m, **data)
         return m
 
-    def process_host_attrs(self, data, host):
+    def process_host_data(self, data, host):
         '''
         Process host snmp attributes
         data is a snmp module object
